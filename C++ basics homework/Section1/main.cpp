f1cce2e4818e812d3123a157308320301d6ac58b
Davii-Ng <nminhd98@gmail.com>
2026-09-01 11:38:26 -0400

    First commit

---
+++ C++ basics homework/Section1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int count = 10;
+    double price = 25.75;
+
+    cout << "The count is: " << count << endl;
+    cout << "The price is: " << price;
+
+    return 0;
+}
