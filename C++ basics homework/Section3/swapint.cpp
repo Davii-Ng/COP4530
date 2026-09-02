c070a63cb38f256b95a56eacf2399cda171a08ba
Davii-Ng <nminhd98@gmail.com>
2026-09-02 15:44:09 -0400

    Section 3 swap ints

---
+++ C++ basics homework/Section3/swapint.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void swapInts(int &a, int &b){
+    int temp;
+    temp = b;
+    b = a;
+    a = temp;
+}
+
+int main(){
+    int a = 6;
+    int b = 7;
+    cout << "Before swap: " << a << " " << b << "\n";
+    swapInts(a, b);
+    cout << "After swap: " << a << " " << b;
+}
