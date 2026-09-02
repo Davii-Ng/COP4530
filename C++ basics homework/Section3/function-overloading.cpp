67accf7c9fb122fb4a5190ea3024b49b3439a470
Davii-Ng <nminhd98@gmail.com>
2026-09-01 21:56:41 -0400

    Section 3 function overloading

---
+++ C++ basics homework/Section3/function-overloading.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int add(int a , int b){
+    return a + b;
+}
+
+int add(int a, int b, int c){
+    return a + b + c;
+}
+
+double add(double a, double b){
+    return a + b;
+}
+
+int main(){
+    // Compiler calls the first version: add(int, int)
+    cout << add(5, 10) << endl;      
+
+    // Compiler calls the second version: add(int, int, int)
+    cout << add(5, 10, 20) << endl;  
+
+    // Compiler calls the third version: add(double, double)
+    cout << add(5.5, 2.3) << endl;   
+
+}
