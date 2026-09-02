a08967048b49752644dedf4cc2bddc5acb09f632
Davii-Ng <nminhd98@gmail.com>
2026-09-02 15:21:58 -0400

    Section 3 default argument

---
+++ C++ basics homework/Section3/default-argument.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+void applyDiscount(double price, double discount = 10.0){
+    cout << "Final Price: " << price * (1- discount /100) << endl;
+}
+
+int main(){
+    applyDiscount(100);
+    applyDiscount(100, 20);
+}
